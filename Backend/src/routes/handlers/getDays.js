510029df15cdff8bf0df1626c3ab833ee2b39a5c
dannyverap <danny.verap@gmail.com>
2023-06-29 18:33:06 -0500

    modificación getDay

--- Backend/src/routes/handlers/getDays.js
+++ Backend/src/routes/handlers/getDays.js
@@ -2,7 +2,7 @@ const {daysController} = require('../controllers/daysController')
 
 module.exports = async(req, res) =>{
   try {
-    const {monthNumber, yearNumber} = req.body
+    const {monthNumber, yearNumber} = req.query
     const monthActuall = daysController(monthNumber, yearNumber)
     res.status(200).json(monthActuall)
   } catch (error) {
