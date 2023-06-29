759e4a20b145b6f694416bceaeb7ddfd31cb94d5
AnthonyMB <tonyaamb2003@gmail.com>
2023-06-29 10:40:10 -0500

    feat(/Backend/src/routes)Added controllers to get months

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,7 +1,10 @@
+const {getMonth} = require('./handlers/getMonth');
+
 const { Router } = require("express");
 
-
 const router = Router();
+
+router.get('/month', getMonth)
 
 
 module.exports = router
