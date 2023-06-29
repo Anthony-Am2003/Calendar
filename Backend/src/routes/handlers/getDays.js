2bc4de4bd606847a9a1eb877c6c1f6dae1da18be
AnthonyMB <tonyaamb2003@gmail.com>
2023-06-29 14:42:29 -0500

    cambio a query de controller

--- Backend/src/routes/handlers/getDays.js
+++ Backend/src/routes/handlers/getDays.js
@@ -1,11 +1,11 @@
-const {daysController} = require('../controllers/daysController')
+const { daysController } = require('../controllers/daysController');
 
-module.exports = async(req, res) =>{
+module.exports = async (req, res) => {
   try {
-    const {monthNumber, yearNumber} = req.body
-    const monthActuall = monthController(monthNumber, yearNumber)
-    res.status(200).json(monthActuall)
+    const { monthNumber, yearNumber } = req.query; 
+    const monthActuall = daysController(monthNumber, yearNumber);
+    res.status(200).json(monthActuall);
   } catch (error) {
-    res.status(500).send(error.message)
+    res.status(500).send(error.message);
   }
-}
+};
