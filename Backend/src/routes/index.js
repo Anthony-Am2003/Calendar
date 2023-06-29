d2c5cea8c05a8438ae3c1837c404f37dcd4e726b
dannyverap <danny.verap@gmail.com>
2023-06-29 12:56:58 -0500

    evento Post

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,5 +1,6 @@
 const getDays = require('./handlers/getDays');
 const getMonthOfYears = require('./handlers/getMonthsOfYear')
+const postEvents = require('./handlers/postEvents')
 
 
 const { Router } = require("express");
@@ -8,5 +9,6 @@ const router = Router();
 
 router.get('/days', getDays)
 router.get('/months', getMonthOfYears)
+router.post('/events' , postEvents)
 
 module.exports = router
