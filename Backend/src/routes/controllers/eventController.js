fe74468c92f2ce5c1a52821af82a48a383cf6aeb
dannyverap <danny.verap@gmail.com>
2023-06-29 18:13:04 -0500

    Post

--- Backend/src/routes/controllers/eventController.js
+++ Backend/src/routes/controllers/eventController.js
@@ -2,7 +2,7 @@ const { Events } = require("../../db.js")
 
 const postEventController = async (name, initialDate, finalDate, image, description, location, reminder, category) => {
 
-    const newEvent = await Events.create({name, initialDate, finalDate, image, description, location, reminder, category})
+    const newEvent = await Events.create({ name, initialDate, finalDate, image, description, location, reminder, category })
 
     return newEvent
 }
