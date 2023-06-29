63e8c9a5eae255a4c462232ebb52184bbf2b9121
dannyverap <danny.verap@gmail.com>
2023-06-29 10:55:29 -0500

    back

--- Backend/src/routes/handlers/getMonth.js
+++ Backend/src/routes/handlers/getMonth.js
@@ -2,7 +2,7 @@ const {monthController} = require('../controllers/monthController')
 
 module.exports = async(req, res) =>{
   try {
-    const {nmonthNumber, yearNumber} = req.body
+    const {monthNumber, yearNumber} = req.body
     const monthActuall = monthController()
     res.status(200).json(monthActuall)
   } catch (error) {
