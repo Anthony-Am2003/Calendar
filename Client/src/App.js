20026f7107bc77fb82916b1021279d106150ea79
franvazquezz <franvazquez2001@gmail.com>
2023-06-30 15:30:31 -0300

    pancho register & login

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import HomePage from "./views/home/home";
-import Form from "./views/form/form";
+import LoginPage from "./views/login/login";
 import {Routes, Route} from "react-router-dom";
+import RegisterPage from "./views/register/register";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/events" element={<Form />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        {/* <Route path="/events" element={<FormView />} /> */}
         <Route path="/calendars" />
       </Routes>
     </div>
