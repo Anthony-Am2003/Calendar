37c87dd679c3a4b73e2daa1fe76571a1aa5a51b2
dannyverap <danny.verap@gmail.com>
2023-06-30 11:08:47 -0500

    Busqueda por Name

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -5,6 +5,7 @@ const postEvents = require("./handlers/postEvents");
 const getAllEvents = require("./handlers/getAllEvents");
 const register = require("./handlers/register");
 const login = require("./handlers/login");
+const getEventsByName = require("./handlers/getEventsByName")
 
 const { Router } = require("express");
 
@@ -15,6 +16,7 @@ router.get("/months", getMonthOfYears);
 router.post("/events", postEvents);
 router.delete("/events/:id", deleteEvent);
 router.get("/events", getAllEvents);
+router.get("/events/name",getEventsByName)
 
 router.post("/user/register", register);
 router.post("/user/login", login);
