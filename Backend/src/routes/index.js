6ac5590fea91ee6156a9f6ac72094325f417a7e4
NoSquazer <joapaez12@gmail.com>
2023-06-30 10:29:16 -0300

    feat(backend/models & backend/controllers)
    Cree un modelo User con sus controladores register & login

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,12 +1,16 @@
-const getDays = require('./handlers/getDays');
-const getMonthOfYears = require('./handlers/getMonthsOfYear')
-
+const getDays = require("./handlers/getDays");
+const getMonthOfYears = require("./handlers/getMonthsOfYear");
+const register = require("./handlers/register");
+const login = require("./handlers/login");
 
 const { Router } = require("express");
 
 const router = Router();
 
-router.get('/days', getDays)
-router.get('/months', getMonthOfYears)
+router.get("/days", getDays);
+router.get("/months", getMonthOfYears);
 
-module.exports = router
+router.post("/user/register", register);
+router.post("/user/login", login);
+
+module.exports = router;
