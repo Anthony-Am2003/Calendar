4b3a49020bac2d34490b71c19b55688372f1936e
franvazquezz <franvazquez2001@gmail.com>
2023-06-30 21:05:24 -0300

    ahora si

--- Client/src/Redux/action-types.js
+++ Client/src/Redux/action-types.js
@@ -7,7 +7,3 @@ export const GET_MONTHS_PER_YEAR = "GET_MONTHS_PER_YEAR"
 export const GET_DETAIL_DAY = "GET_DETAIL_DAY"
 
 export const POST_EVENT = "POST_EVENT"
-
-export const POST_LOGIN = "POST_LOGIN"
-
-export const POST_REGISTER = "POST_REGISTER"
