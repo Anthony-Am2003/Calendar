994d8a22471551893bac5caae1a1b0f05b58e104
Matias Gigena <tute910@gmail.com>
2023-07-02 02:01:57 -0300

    event y eventDetail terminado

--- Client/src/components/EventsDetail/detail.jsx
+++ Client/src/components/EventsDetail/detail.jsx
@@ -2,10 +2,12 @@ import {useEffect, useState} from "react";
 import {useLocation} from "react-router-dom";
 import {getDetail} from "../../Redux/actions";
 import {useDispatch, useSelector} from "react-redux";
+import img from "../../assets/img/pexels-fauxels-3184183.jpg";
+import img2 from "../../assets/img/pexels-greta-hoffman-7859031.jpg";
 
 const EventDetail = () => {
   const dispatch = useDispatch();
-  const [loading, setLoading] = useState(true);
+
   const location = useLocation();
   const name = new URLSearchParams(location.search).get("name");
   const selector = useSelector((state) => state.eventDetail);
@@ -15,141 +17,124 @@ const EventDetail = () => {
     dispatch(getDetail(name));
   }, []);
   return (
-    <section class="text-gray-600 body-font overflow-hidden">
-      <div class="container px-5 py-24 mx-auto">
-        <div class="lg:w-4/5 mx-auto flex flex-wrap">
-          <img
-            alt="ecommerce"
-            class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-            src={selector[0]?.image}
-          />
-          <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-            <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">
-              {selector[0]?.name}
-            </h1>
-            <div class="flex mb-4">
-              <span class="flex items-center">
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  class="w-4 h-4 text-indigo-500"
-                  viewBox="0 0 24 24">
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  class="w-4 h-4 text-indigo-500"
-                  viewBox="0 0 24 24">
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  class="w-4 h-4 text-indigo-500"
-                  viewBox="0 0 24 24">
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  class="w-4 h-4 text-indigo-500"
-                  viewBox="0 0 24 24">
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  class="w-4 h-4 text-indigo-500"
-                  viewBox="0 0 24 24">
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <span class="text-gray-600 ml-3">4 Reviews</span>
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-10 lg:grid-cols-2">
+        <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg">
+          <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-teal-accent-400">
+            <svg className="text-teal-900 w-7 h-7" viewBox="0 0 24 24">
+              <polyline
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                points=" 8,5 8,1 16,1 16,5"
+                strokeLinejoin="round"
+              />
+              <polyline
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                points="9,15 1,15 1,5 23,5 23,15 15,15"
+                strokeLinejoin="round"
+              />
+              <polyline
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                points="22,18 22,23 2,23 2,18"
+                strokeLinejoin="round"
+              />
+
+              <rect
+                x="9"
+                y="13"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                width="6"
+                height="4"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+
+          <div className="max-w-xl mb-6">
+            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+              Let us handle
+              <br className="hidden md:block" />
+              your next {selector[0]?.name} {""}
+              <span className="inline-block text-deep-purple-accent-400">
+                EVENT.
               </span>
-              <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                <a class="text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24">
-                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                  </svg>
-                </a>
-                <a class="text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24">
-                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                  </svg>
-                </a>
-                <a class="text-gray-500">
-                  <svg
-                    fill="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24">
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                  </svg>
-                </a>
-              </span>
+            </h2>
+            <p className="text-base text-gray-700 md:text-lg">
+              Here in Klendar©, our first priority is to satisfy our users. We
+              value your events and understand the importance of providing
+              accurate and meaningful descriptions. Your event, "
+              {selector[0]?.description}", is an integral part of our commitment
+              to delivering a personalized and user-centric experience.
+            </p>
+          </div>
+          <div className="stats overflow-hidden shadow">
+            <div className="stat place-items-center">
+              <div className="stat-title">Events listed</div>
+              <div className="stat-value">31K</div>
+              <div className="stat-desc">From January 1st to December 1st</div>
             </div>
-            <p class="leading-relaxed">{selector[0]?.description}</p>
-            <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-              <div class="flex ml-6 items-center">
-                <div class="relative">
-                  <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center"></span>
-                </div>
-              </div>
+
+            <div className="stat place-items-center">
+              <div className="stat-title">Users</div>
+              <div className="stat-value text-secondary">4,200</div>
+              <div className="stat-desc text-secondary">↗︎ 40 (2%)</div>
             </div>
-            <div class="flex">
-              <span class="title-font font-medium text-2xl text-gray-900">
-                {selector[0]?.hour}
-              </span>
-              <a
-                href="/events"
-                class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                Back
-              </a>
-              <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                <svg
-                  fill="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  class="w-5 h-5"
-                  viewBox="0 0 24 24">
-                  <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                </svg>
-              </button>
+
+            <div className="stat place-items-center">
+              <div className="stat-title">New Registers</div>
+              <div className="stat-value">1,200</div>
+              <div className="stat-desc">↗︎ 90 (14%)</div>
+            </div>
+          </div>
+          <a href="/events" className="btn mt-5 btn-primary">
+            Back
+          </a>
+        </div>
+        <div className="flex items-center justify-center -mx-4 lg:pl-8">
+          <div className="flex flex-col items-end px-3">
+            <img
+              className="object-cover mb-6 rounded shadow-lg h-28 sm:h-56 xl:h-80 w-28 sm:w-48 xl:w-80"
+              src={img2}
+              alt=""
+            />
+            <img
+              className="object-cover w-20 h-20 rounded shadow-lg sm:h-44  xl:h-56 sm:w-40 xl:w-56"
+              src={img}
+              alt=""
+            />
+          </div>
+          <div className="px-3">
+            <img
+              className="object-cover w-40 h-40 rounded shadow-lg sm:h-72 xl:h-96 sm:w-64 xl:w-96"
+              src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+              alt=""
+            />
+            <div className="px-3 py-5">
+              <img
+                className="object-cover w-20 h-20 rounded shadow-lg sm:h-44  xl:h-56 sm:w-40 xl:w-56"
+                src={selector[0]?.image}
+                alt="You have not provided an image."
+              />
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 export default EventDetail;
